0078fa1a62b276ce9fe6ea01758e46a6b8eb91d7
Abbieturner95 <abbieturner95@hotmail.com>
2018-10-24 12:51:13 +0100

    activitys folder + icon (unfinished)

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/MainActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/MainActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 
 import com.example.abbieturner.restaurantsfinder.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
