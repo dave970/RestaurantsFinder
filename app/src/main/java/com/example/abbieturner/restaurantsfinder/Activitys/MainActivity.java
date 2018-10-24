fba95169ba275a232acf57866dd0e1a738b260f0
Abbieturner95 <abbieturner95@hotmail.com>
2018-10-24 12:43:42 +0100

    activitys folder + icon (unfinished)

---
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.abbieturner.restaurantsfinder.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.abbieturner.restaurantsfinder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
