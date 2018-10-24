a47ef332c11ff84fb9515903d686d26405926f1c
Abbieturner95 <abbieturner95@hotmail.com>
2018-10-24 13:40:53 +0100

    Added firebase set up online

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/LogInActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/LogInActivity.java
@@ -12,16 +12,14 @@ import com.example.abbieturner.restaurantsfinder.R;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
     }
-
-
 
 
     @Override
@@ -29,17 +27,17 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             finish();
-            setEnterExitTransition(new Intent(MainActivity.this, CuisineActivity.class));
+            setEnterExitTransition(new Intent(LogInActivity.this, CuisineActivity.class));
         } else {
             finish();
-            startActivity(new Intent(MainActivity.this, CuisineActivity.class));
+            startActivity(new Intent(LogInActivity.this, CuisineActivity.class));
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)  //Transactiom from activity to activity
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)  //Transaction from activity to activity
     private void setEnterExitTransition(Intent intent) {
         getWindow().setExitTransition(new Fade().setDuration(1000));
         getWindow().setReenterTransition(new Fade().setDuration(1000));
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(LogInActivity.this).toBundle());
     }
 }
