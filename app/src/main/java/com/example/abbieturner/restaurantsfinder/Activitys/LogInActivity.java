e37ed2260afb226a18e6389157e8da273a7065c5
Abbieturner95 <abbieturner95@hotmail.com>
2018-10-25 10:29:07 +0100

    added log out activity res and sign in result

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/LogInActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activitys/LogInActivity.java
@@ -2,39 +2,118 @@ package com.example.abbieturner.restaurantsfinder.Activitys;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Fade;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import com.example.abbieturner.restaurantsfinder.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
+import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.Icon;
+import java.util.Arrays;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class LogInActivity extends AppCompatActivity {
+
+    @BindView(R.id.sign_in_btn) Button signin_button;
+
+    private FirebaseAuth mAuth;
+    private static int RC_SIGN_IN = 109;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(Arrays.asList(
+                                        new AuthUI.IdpConfig.GoogleBuilder().build()))    //add phone, facebook?
+                                .build(),
+                        RC_SIGN_IN);
+            }
+        });
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    finish();
+                    setEnterExitTransition(new Intent(LogInActivity.this, CuisineActivity.class));
+                } else {
+                    finish();
+                    startActivity(new Intent(LogInActivity.this, CuisineActivity.class));
+                }
+            } else {
+                if (response == null) {
+                    Toast.makeText(this, R.string.sign_in_cancelled, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    new FancyAlertDialog.Builder(this)
+                            .setTitle(getString(R.string.connection_error))
+                            .setBackgroundColor(Color.parseColor("#b7030f"))
+                            .setMessage(getString(R.string.check_connection))
+                            .setNegativeBtnText(getString(R.string.ok))
+                            .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))
+                            .setAnimation(Animation.POP)
+                            .isCancellable(true)
+                            .setIcon(R.drawable.ic_cancel_black_24dp, Icon.Visible)
+                            .OnNegativeClicked(new FancyAlertDialogListener() {
+                                @Override
+                                public void OnClick() {
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            })
+                            .build();
+                    return;
+                }
+                Toast.makeText(this, R.string.signin_error_trya, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     public void onStart() {
         super.onStart();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            finish();
-            setEnterExitTransition(new Intent(LogInActivity.this, CuisineActivity.class));
-        } else {
-            finish();
-            startActivity(new Intent(LogInActivity.this, CuisineActivity.class));
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                finish();
+                setEnterExitTransition(new Intent(LogInActivity.this, CuisineActivity.class));
+            } else {
+                finish();
+                startActivity(new Intent(LogInActivity.this, CuisineActivity.class));
+            }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)  //Transaction from activity to activity
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)                    //Transaction from activity to activity
     private void setEnterExitTransition(Intent intent) {
         getWindow().setExitTransition(new Fade().setDuration(1000));
         getWindow().setReenterTransition(new Fade().setDuration(1000));
